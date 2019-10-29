0ee66e0d2b579e08514ef947798b907f411d337e
Samuel Pai <samuelpai16@gmail.com>
2019-10-28 22:02:48 -0700

    updates

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require("express");
 var exphbs = require("express-handlebars");
 var app = express();
 app.use(express.static("public"));
-var mongoose = require("mongoose"); 
+var mongoose = require("mongoose");
+ 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/samscraper";
 mongoose.connect(MONGODB_URI);
 
